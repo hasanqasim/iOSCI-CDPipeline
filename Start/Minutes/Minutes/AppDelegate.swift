28e6eec6e911fec78de73b6d074b0c2db5a63da5
Hasan Qasim <hasanqasim92@gmail.com>
2020-09-28 17:30:26 +1000

    add code to start the app center services

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,4 +1,9 @@
 import UIKit
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterPush
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -8,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        MSAppCenter.start("5e536648-c294-438f-98a5-94267668ddea", withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
         return true
     }
 
